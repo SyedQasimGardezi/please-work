c04152c900072be6558da738ff7c55857a1c608f
Syed Qasim <shahq383@gmail.com>
2022-08-21 22:58:10 +0500

    2nd commit

--- js/app.js
+++ js/app.js
@@ -1,9 +1,29 @@
 const pointer = document.querySelector(".cursor");
 const hamburger = document.querySelector(".hamburger");
+const logoSvg = document.querySelector(".logo-svg");
 const headerLinks = document.querySelector(".header-links");
-const prank = document.querySelector(".prank");
+const prank = document.querySelectorAll(".prank");
 const img = document.querySelectorAll(".image");
 const links = document.querySelectorAll(".links");
+const cards = gsap.utils.toArray('.card')
+
+logoSvg.addEventListener('mouseover',()=>{
+    gsap.fromTo('.logo-svg',0.1,{scaleY:1},{scaleY:2})
+})
+logoSvg.addEventListener('mouseleave',()=>{
+    gsap.fromTo('.logo-svg',0.1,{scaleY:2},{scaleY:1})
+})
+cards.forEach(test => {
+    gsap.from(test,0.5,{opacity:0,y:200,scrollTrigger:{
+        trigger:test
+    }});
+});
+gsap.from('.logo-svg',1,{opacity:0,y:-50});
+gsap.from('.hero-left',1,{opacity:0,y:50});
+
+gsap.fromTo('.hero-button',1,{opacity:0},{opacity:1})
+gsap.fromTo('.hero-img',0.5,{clipPath:"circle(0% at 50% 50%)"},{clipPath:"circle(70.5% at 50% 50%)"})
+
 window.addEventListener('mousemove', cursor);
 function cursor(e) {
     pointer.style.top = e.pageY + "px";
@@ -28,9 +48,11 @@ img.forEach(x => {
         pointer.classList.remove('cursor-over-img')
     })
 })
-prank.addEventListener('mouseover', () => {
-    pointer.classList.add('cursor-over-prank')
+prank.forEach(x => {
+    x.addEventListener('mouseover', () => {
+        pointer.classList.add('cursor-over-prank')
+    })
+    x.addEventListener('mouseleave', () => {
+        pointer.classList.remove('cursor-over-prank')
 })
-prank.addEventListener('mouseleave', () => {
-    pointer.classList.remove('cursor-over-prank')
 })
